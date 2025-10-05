bae3ba83daf598a688ad6c50786d9f019f9f1a56
Cameron Morris <cmorris49@my.bcit.ca>
2025-10-04 17:46:18 -0700

    fixed env issue

--- client/src/lib/firebaseConfig.ts
+++ client/src/lib/firebaseConfig.ts
@@ -18,5 +18,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const db = getFirestore(app);
+console.log(firebaseConfig);
 
 export { app, analytics, db };
